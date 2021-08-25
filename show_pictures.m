5ae196b9c85f803e869590104e9695867d5465ae
Maëlle Christiaens <maelle.christiaens@ens.psl.eu>
2021-08-25 16:24:37 +0200

    Commented plot_touch and show_pictures

--- show_pictures.m
+++ show_pictures.m
@@ -1,8 +1,11 @@
 function[fig] = show_pictures(stimuli)
+%this script is not finished
+%the idea was to create a big layout (4 columns, 1 for each phase) and to
+%present which pictures were shown for each trial, and on which of these
+%pictures the subject clicked on
 cd C:\Users\maell\Documents\ENS\Cours\Césure\Stage_Sliwa\MonkeyLogic\human_task_mc\Figures_control\Pictures
-
-fig = figure('Position', [20 40 1200 600]);
-t = tiledlayout(1,2);
+fig = figure('Position', [20 40 1200 600]); 
+t = tiledlayout(1,2); 
 nexttile(t)
 s = tiledlayout(t, 2,2);
 nexttile(s,[1 2])
